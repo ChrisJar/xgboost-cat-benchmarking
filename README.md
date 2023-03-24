21bd91cd3277d7d25b774455444bd4264f1b28ee
Chris Jarrett <cjarrett@ipp1-3302.aselab.nvidia.com>
2023-03-24 12:31:13 -0700

    Address reviews

--- README.md
+++ README.md
@@ -4,3 +4,8 @@
 ```
 mamba create -n xgb-cat-bench -c rapidsai-nightly -c conda-forge python=3.10 cudatoolkit=11.8 rapids-xgboost jupyterlab cudf=23.04 dask-cudf dask-cuda 'ucx-proc=*=gpu' ucx-py
 ```
+
+### Memory Usage Stats
+Using the experimental categorical feature currently uses about 78GB of GPU memory on 3/40th of the Criteo Dataset. So, it should roughly take 14 80GB A100s to train on the full dataset.
+
+TODO: Get an estimate for GPUs needed without using the experimental categorical feature.
--- e2e-cat.py
+++ e2e-cat.py
@@ -12,64 +12,68 @@ from sklearn.metrics import accuracy_score, log_loss
 train_data_dir = "/datasets/criteo/crit_orig_pq_10days"
 test_data_dir = "/raid/cjarrett/data/criteo/crit_orig_pq_1day"
 
-cluster = LocalCUDACluster(
-    n_workers=1,
-    CUDA_VISIBLE_DEVICES="2",
-    protocol="ucx",
-    jit_unspill=True,
-    rmm_pool_size="70GiB",
-    device_memory_limit="78GB",
-)
-client = Client(cluster)
+def main():
+    cluster = LocalCUDACluster(
+        n_workers=1,
+        protocol="ucx",
+        jit_unspill=True,
+        rmm_pool_size="70GiB",
+        device_memory_limit="78GB",
+    )
+    client = Client(cluster)
 
-train_ddf = dask_cudf.read_parquet(train_data_dir, split_row_groups=True)
+    train_ddf = dask_cudf.read_parquet(train_data_dir, split_row_groups=True)
 
-def transform_df(ddf):
-    ddf = ddf.fillna(0)
+    def transform_df(ddf):
+        ddf = ddf.fillna(0)
 
-    for n in range(1,27):
-        col = "C" + str(n)
-        ddf[col] = ddf[col].astype("category")
-    return ddf
+        for n in range(1,27):
+            col = "C" + str(n)
+            ddf[col] = ddf[col].astype("category")
+        return ddf
 
-train_ddf = transform_df(train_ddf)
-X_train, y_train = train_ddf.drop("label", axis=1), train_ddf["label"]
-del train_ddf
+    train_ddf = transform_df(train_ddf)
+    X_train, y_train = train_ddf.drop("label", axis=1), train_ddf["label"]
+    del train_ddf
 
-dtrain = xgb.dask.DaskQuantileDMatrix(client, X_train, y_train, enable_categorical=True)
-del X_train
-del y_train
+    dtrain = xgb.dask.DaskQuantileDMatrix(client, X_train, y_train, enable_categorical=True)
+    del X_train
+    del y_train
 
-params = {
-    "tree_method": "gpu_hist",
-    "objective": "binary:logistic",
-    "max_cat_to_onehot": 1
-}
+    params = {
+        "tree_method": "gpu_hist",
+        "objective": "binary:logistic",
+        "max_cat_to_onehot": 1
+    }
 
-t0 = time.time()
-output = xgb.dask.train(
-        client,
-        params,
-        dtrain,
-        num_boost_round=100,
-        evals=[(dtrain, "train")],
-)
-t1 = time.time()
-print("Training Time: {}".format(t1-t0))
+    t0 = time.time()
+    output = xgb.dask.train(
+            client,
+            params,
+            dtrain,
+            num_boost_round=100,
+            evals=[(dtrain, "train")],
+    )
+    t1 = time.time()
+    print("Training Time: {}".format(t1-t0))
 
-del dtrain
+    del dtrain
 
-test_ddf = dd.read_parquet(test_data_dir, split_row_groups=True)
+    test_ddf = dd.read_parquet(test_data_dir, split_row_groups=True)
 
-test_ddf = transform_df(test_ddf)
-X_test, y_test = test_ddf.drop("label", axis=1), test_ddf["label"]
-del test_ddf
+    test_ddf = transform_df(test_ddf)
+    X_test, y_test = test_ddf.drop("label", axis=1), test_ddf["label"]
+    del test_ddf
 
-y_test_pred_prob = xgb.dask.inplace_predict(client, output, X_test)
-y_test_pred_val = y_test_pred_prob>=0.5
+    y_test_pred_prob = xgb.dask.inplace_predict(client, output, X_test)
+    y_test_pred_val = y_test_pred_prob>=0.5
 
-test_acc = accuracy_score(y_test.compute(), y_test_pred_val.compute())
-test_log_loss = log_loss(y_test.compute(), y_test_pred_prob.compute())
+    test_acc = accuracy_score(y_test.compute(), y_test_pred_val.compute())
+    test_log_loss = log_loss(y_test.compute(), y_test_pred_prob.compute())
 
-print("Test Accuracy: {}".format(test_acc))
-print("Test Log Loss: {}".format(test_log_loss))
+    print("Test Accuracy: {}".format(test_acc))
+    print("Test Log Loss: {}".format(test_log_loss))
+
+
+if __name__ == "__main__":
+    main()
